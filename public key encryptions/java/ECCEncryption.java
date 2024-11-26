680a164d4ac179a2afcb38c20802206f69ec11b9
Olivia Polos <38380599+odp31@users.noreply.github.com>
2024-11-26 10:18:15 -0500

    Create ECCEncryption.java

---
+++ public key encryptions/java/ECCEncryption.java
@@ -0,0 +1,6 @@
+// elliptic curve cryptography
+// based on mathematics of elliptic curves
+// offers similar security to RSA but with smaller key sizes
+// key length - 256 or 384 bits 
+// use cases- secure communication, digital signatures, key exchange 
+
