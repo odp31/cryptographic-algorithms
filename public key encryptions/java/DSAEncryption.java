15cb3b91ec5f3538d9708e5f662551cbe9e23a51
Olivia Polos <38380599+odp31@users.noreply.github.com>
2024-11-26 10:17:22 -0500

    Create DSAEncryption.java

---
+++ public key encryptions/java/DSAEncryption.java
@@ -0,0 +1,6 @@
+// Digital Signature Algorithm
+// primarily used for digital signatures 
+// generates a pair of keys; public and private
+// signing: message is hashed, hash is signed using the private key
+// verification: signature is verified using public key 
+
