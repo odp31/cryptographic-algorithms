5a7797c121e35eae437e38c665ca353f45e2789d
Olivia Polos <38380599+odp31@users.noreply.github.com>
2024-11-26 10:30:50 -0500

    Update DESEncryption.java

--- private key encryptions/java/DESEncryption.java
+++ private key encryptions/java/DESEncryption.java
@@ -1,3 +1,27 @@
 // data encryption standard
 // older and less secure
 // block cipher with a 56-bit key
+
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+public class DESEncryption
+  {
+    public static byte[] encrypt(byte[] plaintext, byte[] key) throws Exception
+    {
+      Cipher cipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
+      SecretKeySpec keySpec = new SecretKeySpec(key, "DES");
+      cipher.init(Cipher.ENCRYPT_MODE, keySpec);
+      byte[] ciphertext = cipher.doFinal(plaintext);
+      return ciphertext;
+    }
+    public static byte[] decrypt(byte[] ciphertext, byte[] key) throws Exception
+    {
+      Cipher cipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
+      SecretKeySpec keySpec = new SecretKeySpec(key, "DES");
+      cipher.init(Cipher.DECRYPT_MODE, keySpec);
+      byte[] plaintext = cipher.doFinal(ciphertext);
+      return plaintext;
+    }
+  }
