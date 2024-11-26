72cfb8d562eda7e049a96d2bae65646fde70a18f
Olivia Polos <38380599+odp31@users.noreply.github.com>
2024-11-26 10:38:02 -0500

    Update ECCEncryption.java

--- public key encryptions/java/ECCEncryption.java
+++ public key encryptions/java/ECCEncryption.java
@@ -4,3 +4,21 @@
 // key length - 256 or 384 bits 
 // use cases- secure communication, digital signatures, key exchange 
 
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import java.security.*;
+import javax.crypto.Cipher;
+
+public class ECCEncryption
+  {
+    public static byte[] encrypt(byte[] plaintext, PublicKey publicKey) throws Exception
+    {
+      Security.addProvider(new BouncyCastleProvider());
+
+      Cipher cipher = Cipher.getInsatnce("ECIES", "BC");
+      cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+
+      byte[] ciphertext = cipher.doFinal(plaintext);
+      return ciphertext;
+    }
+  }
