bc730c8db0c6866d947ee6cdb2f2e912ea078ef0
Olivia Polos <38380599+odp31@users.noreply.github.com>
2024-11-26 10:16:29 -0500

    Create RSAEncryption.java

---
+++ public key encryptions/java/RSAEncryption.java
@@ -0,0 +1,4 @@
+// generates a pair of keys, public and private
+// encyrption: plaintext is encrypted using the public key
+// decryption: ciphertext is decrypted using the private key
+// use cases: secure communication, digital signatures, key exchange 
