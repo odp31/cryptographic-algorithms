4afab8f4cc2415ff0bbf613e54772fdce0c0653f
Olivia Polos <38380599+odp31@users.noreply.github.com>
2024-11-26 10:40:46 -0500

    Update RSAEncryption.java

--- public key encryptions/java/RSAEncryption.java
+++ public key encryptions/java/RSAEncryption.java
@@ -2,3 +2,24 @@
 // encyrption: plaintext is encrypted using the public key
 // decryption: ciphertext is decrypted using the private key
 // use cases: secure communication, digital signatures, key exchange 
+
+import java.security.*;
+import javax.crypto.Cipher;
+
+public class RSAEncryption
+  {
+    public static byte[] encrypt(byte[] plaintext, PublicKey publicKey) throws Exception
+    {
+      Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA256AndMGF1Padding");
+      cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+      byte[] ciphertext = cipher.doFinal(plaintext);
+      return ciphertext;
+    }
+    public static byte[] decrypt(byte[] ciphertext, PrivateKey privateKey) throws Exception
+    {
+      Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA256AndMGF1Padding");
+      cipher.init(Cipher.DECRYPT_MODE, privateKey);
+      byte[] plaintext = cipher.doFinal(ciphertext);
+      return plaintext;
+    }
+  }
